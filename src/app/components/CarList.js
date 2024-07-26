b645af54877c7d895ddd6747ed051997fb5972ec
sulthanakeysie <sulthanakeysi@gmail.com>
2024-07-26 08:32:12 +0400

    added no car available screen

--- src/app/components/CarList.js
+++ src/app/components/CarList.js
@@ -29,33 +29,39 @@ const CarList = ({ cars }) => {
 
   return (
     <div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-        {cars.map((car) => (
-          <div
-            key={car.id}
-            className="border rounded-2xl shadow-lg bg-[#FFF] cursor-pointer"
-            onClick={() => openModal(car)}
-          >
-            <Image
-              src={`/cars/${car.image}`}
-              alt={car.brandName}
-              width={100}
-              height={100}
-              style={{ width: "100%", height: "200px" }}
-              className="rounded-t-2xl"
-            />
-            <div className="p-6">
-              <h2 className="text-xl font-bold">
-                {car.carName} - {car.date}
-              </h2>
-              <p className="mb-1 text-[#CCC]">{car.id}</p>
-              <hr />
-              <p className="mt-2">Brand: {car.brandName}</p>
-              <p>Model: {car.modelName}</p>
+      {cars.length === 0 ? (
+        <div className="flex items-center justify-center h-[65vh]">
+          <p className="text-xl font-bold text-gray-500">No cars available</p>
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+          {cars.map((car) => (
+            <div
+              key={car.id}
+              className="border rounded-2xl shadow-lg bg-[#FFF] cursor-pointer"
+              onClick={() => openModal(car)}
+            >
+              <Image
+                src={`/cars/${car.image}`}
+                alt={car.brandName}
+                width={100}
+                height={100}
+                style={{ width: "100%", height: "200px" }}
+                className="rounded-t-2xl"
+              />
+              <div className="p-6">
+                <h2 className="text-xl font-bold">
+                  {car.carName} - {car.date}
+                </h2>
+                <p className="mb-1 text-[#CCC]">{car.id}</p>
+                <hr />
+                <p className="mt-2">Brand: {car.brandName}</p>
+                <p>Model: {car.modelName}</p>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
 
       {isModalOpen && selectedCar && (
         <div
